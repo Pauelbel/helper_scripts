49633a941b2e7a9e838a154affc4faaf42f24361
pvb.belov <pvb.belov@gmail.com>
2024-07-11 15:46:31 +0300

    убрал лишнее и не рабочее

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
 [![Total Lines of Code](https://tokei.rs/b1/github/Pauelbel/Helper-Scripts?category=lines)](https://github.com/Pauelbel/Helper-Scripts)
 
-# Helper-Scripts
-
-**Helper-Scripts** - Набор скриптов которые могут быть полезны для тестирования ПО
-
-## Список скриптов
-- image_comparison - поиск различий между двумя изображениями
-- line_in_code - подсчет количества не пустых строк в .py файлах внутри директории
--- image_comparison/script.py
+++ image_comparison/script.py
@@ -2,35 +2,42 @@
 Скрипт для сравнения двух изображений и отображения разницы между ними.
 
 Зависимости: 
-- pip install Pillow (написан на pillow-10.3.0)
-- pip install numpy  (numpy-1.26.4)
+
+- pip install Pillow
+- pip install numpy
 
 """
+
 from PIL import Image
 import numpy
 
-class Config:
-    """ Класс для хранения конфигурационных параметров """
 
-    expectation_path = './image_comparison/expected_screenshot.png'
-    actual_path = './image_comparison/actual_screenshot.png'
-    difference_path = './image_comparison/output_difference_screenshot.png'
+class Config:
+    """Класс для хранения конфигурационных параметров"""
+
+    expectation_path = "./image_comparison/expected_screenshot.png"
+    actual_path = "./image_comparison/actual_screenshot.png"
+    difference_path = "./image_comparison/output_difference_screenshot.png"
 
 
 class ImageLoader:
-    """ Класс для загрузки изображений """
+    """Класс для загрузки изображений"""
+
     def __init__(self, image_path):
         self.image_path = image_path
         self.image = None
 
     def load_image(self):
-        self.image = Image.open(self.image_path).convert('RGB')
+        self.image = Image.open(self.image_path).convert("RGB")
         return self.image
 
+
 class ImageComparer:
-    """ Класс для сравнения двух изображений """
+    """Класс для сравнения двух изображений"""
+
     def __init__(self, img1, img2):
-        if img1.size != img2.size: raise ValueError("Изображения разного размера.")
+        if img1.size != img2.size:
+            raise ValueError("Изображения разного размера.")
         self.img1 = img1
         self.img2 = img2
 
@@ -42,8 +49,10 @@ class ImageComparer:
         difference_percentage = (numpy.sum(mask) / mask.size) * 100
         return mask, difference_percentage
 
+
 class ImageDifferencer:
-    """ Класс для отображения разницы между двумя изображениями """
+    """Класс для отображения разницы между двумя изображениями"""
+
     @staticmethod
     def apply_overlay(img, mask):
         result_array = numpy.array(img.copy())
@@ -51,8 +60,10 @@ class ImageDifferencer:
         result_array[mask] = color_rgb
         return result_array
 
+
 class DifferenceImageGenerator:
-    """ Класс для генерации изображения с разницей между ними """
+    """Класс для генерации изображения с разницей между ними"""
+
     def __init__(self, img1, img2, difference_path):
         self.comparer = ImageComparer(img1, img2)
         self.difference_path = difference_path
@@ -64,7 +75,6 @@ class DifferenceImageGenerator:
         result_image.save(self.difference_path)
         return difference_percentage
 
-# Example usage
 if __name__ == "__main__":
 
     expectation_loader = ImageLoader(Config.expectation_path)
@@ -73,6 +83,10 @@ if __name__ == "__main__":
     expectation_image = expectation_loader.load_image()
     actual_image = actual_loader.load_image()
 
-    difference_generator = DifferenceImageGenerator(expectation_image, actual_image, Config.difference_path)
+    difference_generator = DifferenceImageGenerator(
+        expectation_image, actual_image, Config.difference_path
+    )
     difference_percentage = difference_generator.generate_difference_image()
-    print(f"\nСгенерировано финальное изображение, разница между ожиданием и фактом = {difference_percentage:.2f}%\n")
+    print(
+        f"\nСгенерировано финальное изображение, разница между ожиданием и фактом = {difference_percentage:.2f}%\n"
+    )
